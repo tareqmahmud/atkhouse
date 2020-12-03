9200a13b552c6cf9e3742b13025c544d12c962e5
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2020-12-03 21:12:34 +0600

    Add raspberry pi

---
+++ raspberryPI/config.sample.py
@@ -0,0 +1,8 @@
+def fireConfig():
+    return {
+        "apiKey": "",
+        "authDomain": "",
+        "databaseURL": "",
+        "storageBucket": "",
+        "serviceAccount": "./serviceAccountKey.json"
+    }
