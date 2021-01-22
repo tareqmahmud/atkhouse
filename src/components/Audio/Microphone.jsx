1043c1292b5161c678433936994b5107a772b86f
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-01-22 14:35:28 +0600

    Add remaining code

---
+++ src/components/Audio/Microphone.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/Microphone.css';
+import MicrophoneOff from '../../images/MicrophoneOff.svg';
+import MicrophoneOn from '../../images/MicrophoneOn.svg';
+
+const Microphone = (props) => {
+    return (
+        <div className="google-microphone">
+            {props.audioStatus ? <img src={MicrophoneOn} alt="Microphone On"
+                                onClick={props.stopListening}/>
+                : <img src={MicrophoneOff} alt="Microphone Off" onClick={props.startListening}/>}
+        </div>
+    )
+};
+
+export default Microphone;
