177320931f1e96a9dedb06585eb67758b1d6f77a
kafi <abdullahkafi996@gmail.com>
2021-02-05 22:21:58 +0600

    Rewrite signup component

---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,168 @@
+import React, {useCallback, useState} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {Link} from 'react-router-dom'
+import RootContainer from '../Layout/RootContainer';
+import firebase from '../../config/firebase';
+import {CircularProgress} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+export default function SignUp({history}) {
+    const classes = useStyles();
+    const [loader, setLoader] = useState(false);
+
+    const handleSignUp = useCallback(async event => {
+        event.preventDefault();
+        const {email, password} = event.target.elements;
+        try {
+            // Enable the loader
+            setLoader(true);
+
+            // Signup
+            const authUser = await firebase
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+
+            // Fetch the signup user id
+            const currentUserId = authUser.user.uid;
+
+            // Add default light status data
+            await firebase.database().ref('/LedStatus').child(currentUserId).set({
+                'light_status': false
+            })
+
+            history.push('/');
+        } catch (error) {
+            alert(error);
+
+            // Disable the loader
+            setLoader(false);
+        }
+    }, [history]);
+
+    const renderLoader = () => {
+        if (loader) {
+            return (
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    disabled={false}
+                >
+                    <CircularProgress color={'secondary'}/>
+                </Button>
+            )
+        }
+
+        return (
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+            >
+                Sign Up
+            </Button>
+        );
+    }
+
+    return (
+        <RootContainer>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <form onSubmit={handleSignUp} className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="fname"
+                                    name="firstName"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="lname"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                        </Grid>
+
+                        {renderLoader()}
+
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link to="/signin" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        </RootContainer>
+    );
+}
