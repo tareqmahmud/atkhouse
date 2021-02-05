b1f568c714dbea696c61c02a84444e5ab50a159a
Ashiqur Rahman Chowdhury Adib <adibmd50@gmail.com>
2021-02-05 12:24:48 +0600

    Fixed signin bug

---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,117 @@
+import React, {useCallback, useContext} from 'react'; // add {useCallback, useContext}
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {Redirect} from 'react-router';
+import app from '../../config/firebase';
+import {AuthContext} from '../../context/AuthContext';
+
+import {Link} from 'react-router-dom'
+import RootContainer from '../Layout/RootContainer';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+
+export default function SignIn({history}) {
+    const classes = useStyles();
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault();
+            const {email, password} = event.target.elements;
+            try {
+                await app
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                history.push('/');
+            } catch (error) {
+                alert(error);
+            }
+        },
+        [history]
+    );
+    const {currentUser} = useContext(AuthContext);
+    if (currentUser) {
+        return <Redirect to="/"/>;
+    }
+    return (
+        <RootContainer>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form onSubmit={handleLogin} className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary"/>}
+                            label="Remember me"
+                            style={{height: 0, marginTop: '20px'}}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item>
+                                <Link to="/signup" variant="body2">
+                                    {'Don\'t have an account? Sign Up'}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        </RootContainer>
+    );
+}
